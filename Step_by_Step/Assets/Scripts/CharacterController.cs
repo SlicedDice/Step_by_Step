3d7e2a647723a4aeac6619073f7340486c9a0e4f
SlicedDice <60396325+SlicedDice@users.noreply.github.com>
2020-12-29 19:06:38 +0100

    Finished Test Character
    The test Player Character has been finished. Potentially still requires fine-tuning, but that can be worked on in future versions.
    For now, the character can walk and balance properly.

--- Step_by_Step/Assets/Scripts/CharacterController.cs
+++ Step_by_Step/Assets/Scripts/CharacterController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CharacterController : MonoBehaviour
 {
@@ -9,16 +10,33 @@ public class CharacterController : MonoBehaviour
     private Rigidbody mainRB; //Rigidbody of the main body. Will be initialized in Start()
     public float balanceThrust = 1f; //Variable affecting how strong the character balances
 
+    public GameObject rightFoot; //Object Refernece to the stilt's right foot
+    public GameObject leftFoot; //Object Refernece to the stilt's left foot
+    public float feetMassBase = 50f; //The base weight of the feet, changes how strong the character is held on the ground
+    public float footWeightWhileMoving = 5f; //Exactly as the variable name describes, this is the variable effecting how heavy the foot is while moving a stilt
+
+    public GameObject stiltControllerLeft;
+    public GameObject stiltControllerRight; //Two Variables referencing the small object used to control the stilts
+
+    private Vector2 lastMousePos = new Vector2(); //The last position of the mouse; to calculate the mouse movement
+
     // Start is called before the first frame update
     void Start()
     {
         mainRB = mainBody.GetComponent<Rigidbody>();
+        lastMousePos = Input.mousePosition;
+        leftFoot.GetComponent<Rigidbody>().mass = feetMassBase;
+        rightFoot.GetComponent<Rigidbody>().mass = feetMassBase;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         balanceControls();
+        walkingControls();
+        lastMousePos = Input.mousePosition;
     }
 
     //Function for balancing the character
@@ -40,7 +58,48 @@ public class CharacterController : MonoBehaviour
         {
             mainRB.AddForce(transform.right * balanceThrust);
         }
-   
+    }
 
+    //Function for controlling the individual stilts
+    void walkingControls()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 deltaMouseMov = mouseMov(); 
+            Rigidbody leftFootRB = leftFoot.GetComponent<Rigidbody>();
+            Rigidbody leftStiltContr = stiltControllerLeft.GetComponent<Rigidbody>();
+
+            leftFootRB.mass = footWeightWhileMoving;
+            leftFootRB.AddForce(transform.forward * -deltaMouseMov.y * 2);
+            leftFootRB.AddForce(transform.right * -deltaMouseMov.x * 2);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector2 deltaMouseMov = mouseMov();
+            Rigidbody rightFootRB = rightFoot.GetComponent<Rigidbody>();
+            Rigidbody rightStiltContr = stiltControllerRight.GetComponent<Rigidbody>();
+
+            rightFootRB.mass = footWeightWhileMoving;
+            rightFootRB.AddForce(transform.forward * -deltaMouseMov.y * 2);
+            rightFootRB.AddForce(transform.right * -deltaMouseMov.x * 2);
+        }
+        else
+        {
+            leftFoot.GetComponent<Rigidbody>().mass = feetMassBase;
+            rightFoot.GetComponent<Rigidbody>().mass = feetMassBase;
+        }
+        
+    }
+    
+
+    //Function to calculate and return the movement of the mouse relative to the last update
+    Vector2 mouseMov()
+    {
+        Vector2 currMousePos = Input.mousePosition;
+        Vector2 deltaMousePos = lastMousePos - currMousePos;
+        lastMousePos = currMousePos;
+        
+        return deltaMousePos;
     }
 }
