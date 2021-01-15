4f9a086704a9b2985ab430afad9b246f5ec12ca0
SlicedDice <60396325+SlicedDice@users.noreply.github.com>
2021-01-15 22:41:07 +0100

    Added basic sound effects
    Added Sound assets
    Added Background Music
    Added Walking Sounds

---
+++ Step_by_Step/Assets/Scripts/WalkingSound.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public List<AudioClip> hardSurface = new List<AudioClip>();
+    public List<AudioClip> swampSurface = new List<AudioClip>();
+    public List<AudioClip> wetSurface = new List<AudioClip>();
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!Input.GetMouseButton(0) && !Input.GetMouseButton(1))
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                audioSource.clip = randomClip(hardSurface);
+                audioSource.Play();
+            }
+            else if (collision.gameObject.tag == "Swamp Ground")
+            {
+                audioSource.clip = randomClip(swampSurface);
+                audioSource.Play();
+            }
+            else if (collision.gameObject.tag == "Wet Ground")
+            {
+                audioSource.clip = randomClip(wetSurface);
+                audioSource.Play();
+            }
+        }
+    }            
+
+
+    AudioClip randomClip(List<AudioClip> clipList)
+    {
+        int randomNumber = Random.Range(0, clipList.Count);
+        return clipList[randomNumber];
+    }
+}
